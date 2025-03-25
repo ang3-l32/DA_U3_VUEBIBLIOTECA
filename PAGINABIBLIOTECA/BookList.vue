a46d65cdc4ee9c351dcb4e2cda94a3ceb114e5fa
Sandra Yolotzin <80065142+Sandra631@users.noreply.github.com>
2025-03-25 00:41:17 -0600

    Update BookList.vue

--- PAGINABIBLIOTECA/BookList.vue
+++ PAGINABIBLIOTECA/BookList.vue
@@ -20,7 +20,9 @@
   const books = ref([
     { id: 1, title: "El principito", author: "Antoine de Saint-Exupéry", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMab9s-8airRwyzGQuyQoaX0wcTtobkNfb7w&s" },
     { id: 2, title: "Cien años de soledad", author: "Gabriel García Márquez", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwSsR0pOIFUZYQivZGvfV1a74s4Khz8GrbjA&s" },  
-    { id: 3, title: "1984", author: "George Orwell", image: "" }
+    { id: 3, title: "Orgullo y prejuicio", author: "Jane Austen", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6-I0Z_Hg_b25lCxQml5LMXwQV7JKvMFGEHA&s" },  
+    { id: 4, title: "Crónica de una muerte anunciada", author: "Gabriel García Márquez.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8AC7WcUgXpxwyvzqape473lUnPzYdEc9o_w&s" },    
+    { id: 5, title: "1984", author: "George Orwell", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRASKlNdsq5m9x7q7I1TbwTjn9DqxBXZ1x3vw&s" }
 ])
   </script>
   
